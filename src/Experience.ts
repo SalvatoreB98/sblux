81bb9b9d71ffb8c0052ba347345d483068bd5526
Salvatore Butera <salvatore.butera1998@gmail.com>
2025-01-18 14:13:32 +0100

    TryOut render

--- src/Experience.ts
+++ src/Experience.ts
@@ -4,14 +4,22 @@ import { Controls } from './components/Controls';
 
 
 export class Experience {
+  canvas: HTMLCanvasElement;
+
   constructor() {
+    this.canvas = document.querySelector<HTMLCanvasElement>('#webglCanvas')!;
     this.renderShowRoom();
   }
-  renderShowRoom(){
-    const canvas = document.querySelector<HTMLCanvasElement>('#webglCanvas')!;
-    const sceneInstance = new Scene(canvas);
+
+  renderShowRoom() {
+    const sceneInstance = new Scene(this.canvas);
     new Glasses(sceneInstance.scene);
     new Controls(sceneInstance.camera, sceneInstance.renderer);
+    sceneInstance.animate();
+  }
+
+  renderTryOn() {
+    const sceneInstance = new Scene(this.canvas);
     sceneInstance.animate();
   }
 }
