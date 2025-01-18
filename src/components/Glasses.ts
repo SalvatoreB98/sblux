9c1e536aba0a820ccc3baf0d0291b4f4589d0d39
Salvatore Butera <salvatore.butera1998@gmail.com>
2025-01-18 01:27:04 +0100

    Personalize glasses feature

--- src/components/Glasses.ts
+++ src/components/Glasses.ts
@@ -6,32 +6,47 @@ export class Glasses {
 
   constructor(private scene: THREE.Scene) {
     this.loadModel();
+    this.colorsControls();
   }
 
   private loadModel() {
     const loader = new GLTFLoader();
     loader.load('/models/glasses.glb', (gltf) => {
-        this.model = gltf.scene;
-        this.model.scale.set(1, 1, 1);
-        
-        console.log(this.model); // Debugging: Check object structure in console
-        
-        this.model.traverse((child) => {
-            if (child instanceof THREE.Mesh) {
-              console.log(child)
-                if (child.name.includes("Glasses2")) {  // Modify the frame material
-                    child.material = new THREE.MeshStandardMaterial({
-                        color: 0x000000,  // Black frame
-                        metalness: 0.6,
-                        roughness: 0.2
-                    });
-                }
-            }
-        });
+      this.model = gltf.scene;
+      this.model.scale.set(1, 1, 1);
 
-        this.scene.add(this.model);
+      console.log(this.model); // Debugging: Check object structure in console
+
+      this.model.traverse((child) => {
+        if (child instanceof THREE.Mesh) {
+          console.log(child)
+          if (child.name.includes("Glasses_Glasses")) {  // Modify the frame material
+            child.material = new THREE.MeshStandardMaterial({
+              color: 0x000000,  // Black frame
+              metalness: 0.9,
+              roughness: 0.1
+            });
+          }
+        }
+      });
+
+      this.scene.add(this.model);
     }, undefined, (error) => {
-        console.error('Error loading glasses model:', error);
+      console.error('Error loading glasses model:', error);
     });
-}
+  }
+  colorsControls() {
+    document.querySelectorAll('.color-btn').forEach((button) => {
+      button.addEventListener('click', (event) => {
+        const color = (event.target as HTMLElement).getAttribute('data-color');
+        if (color) {
+          this.model?.traverse((child) => {
+            if (child instanceof THREE.Mesh && child.name.includes("Glasses_Glasses")) {
+              child.material.color.set(color);
+            }
+          });
+        }
+      });
+    });
+  }
 }
